b6589655caa300f9f81a1946b6fc5216f34ca96a
Ponacko <ponacko@gmail.com>
2024-09-03 10:34:00 +0200

    implement simple grid in the LayoutManager

--- app/src/main/java/com/example/snaprecycler/GridLayoutManager.kt
+++ app/src/main/java/com/example/snaprecycler/GridLayoutManager.kt
@@ -8,12 +8,42 @@ class GridLayoutManager(
     private val numColumns: Int
 ) : RecyclerView.LayoutManager() {
 
-    override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
-        return RecyclerView.LayoutParams(
+    override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams =
+        RecyclerView.LayoutParams(
             ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
-    }
 
+    override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
+        if (itemCount == 0) {
+            return
+        }
+        val totalWidth = width
+        val totalHeight = height
+        val itemWidth = totalWidth / numColumns
+        val itemHeight = totalHeight / numRows
+
+        var leftOffset = 0
+        var topOffset = 0
+
+        for (position in 0 until itemCount) {
+            val view = recycler.getViewForPosition(position)
+            addView(view)
+
+            val left = leftOffset
+            val right = left + width
+            val top = topOffset
+            val bottom = top + itemHeight
+            measureChildWithMargins(view, 0, 0)
+            layoutDecorated(view, left, top, right, bottom)
+
+            // calculate new offsets
+            leftOffset += itemWidth
+            if ((position + 1) % numColumns == 0) {
+                leftOffset = 0
+                topOffset += itemHeight
+            }
+        }
+    }
 
 }
